a6b0d2aa95f923843f13cec26bfec76a1a8001f7
jmartinor <43027862+jmartinor@users.noreply.github.com>
2025-05-28 19:55:27 +0200

    Adding more options

--- src/allpowers_ble/models.py
+++ src/allpowers_ble/models.py
@@ -9,8 +9,19 @@ class AllpowersState:
 
     ac_on: bool = False
     dc_on: bool = False
+    close_ble: bool = False
+    f60hz_on: bool = False
+    beep_on: bool = False
     light_on: bool = False
+    screen_on: bool = False
+    voice_on: bool = False
     percent_remain: int = 0
     minutes_remain: int = 0
     watts_import: int = 0
     watts_export: int = 0
+    ecoMode_on: bool = False
+    chargingMode: int = 0
+    acMode_on: bool = False
+    carPortEn_on: bool = False
+    ecoTime: int = 0
+    chargeTime: int = 0
