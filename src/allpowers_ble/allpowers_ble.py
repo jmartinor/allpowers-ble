4306e8758a3cdc6b65bb9b1147fb281dd544bf95
jmartinor <43027862+jmartinor@users.noreply.github.com>
2025-05-28 19:53:52 +0200

    Allowing new configs. Second message is processed for ECO Mode, Charging mode, etc

--- src/allpowers_ble/allpowers_ble.py
+++ src/allpowers_ble/allpowers_ble.py
@@ -47,6 +47,7 @@ class AllpowersBLE:
         ble_device: BLEDevice,
         advertisement_data: AdvertisementData | None = None,
     ) -> None:
+#        _LOGGER.setLevel(logging.DEBUG)
         """Init the Allpowers BLE."""
         self._ble_device = ble_device
         self._advertisement_data = advertisement_data
@@ -105,9 +106,64 @@ class AllpowersBLE:
         return self._state.dc_on
 
     @property
+    def f60hz_on(self) -> bool:
+        """Return the state of DC."""
+        return self._state.f60hz_on
+
+    @property
+    def beep_on(self) -> bool:
+        """Return the state of DC."""
+        return self._state.beep_on
+
+    @property
     def light_on(self) -> bool:
         """Return the state of Light."""
         return self._state.light_on
+
+    @property
+    def screen_on(self) -> bool:
+        """Return the state of Light."""
+        return self._state.screen_on
+
+    @property
+    def voice_on(self) -> bool:
+        """Return the state of Light."""
+        return self._state.voice_on
+
+    @property
+    def close_ble(self) -> bool:
+        """Return the state of Light."""
+        return self._state.close_ble
+
+    @property
+    def ecoMode_on(self) -> bool:
+        """Return the state of Light."""
+        return self._state.ecoMode_on
+
+    @property
+    def chargingMode(self) -> int:
+        """Return the state of Light."""
+        return self._state.chargingMode
+
+    @property
+    def acMode_on(self) -> bool:
+        """Return the state of Light."""
+        return self._state.acMode_on
+
+    @property
+    def carPortEn_on(self) -> bool:
+        """Return the state of Light."""
+        return self._state.carPortEn_on
+
+    @property
+    def ecoTime(self) -> int:
+        """Return the state of Light."""
+        return self._state.ecoTime
+
+    @property
+    def chargeTime(self) -> int:
+        """Return the state of Light."""
+        return self._state.chargeTime
 
     @property
     def percent_remain(self) -> int:
@@ -129,6 +185,15 @@ class AllpowersBLE:
         """Return outgoing power in watts."""
         return self._state.watts_export
 
+    def crc(self,data):
+        if len(data) < 2:
+            raise ValueError("The array must have 2 or more elements")
+    
+        a = data[0]
+        for s in range(1, len(data) - 1):
+            a ^= data[s]
+        return a
+
     async def _change_status_to_device(self) -> None:
         """Send the current state back to the device."""
         full = bytes.fromhex("a56500b10101000071")
@@ -137,16 +202,41 @@ class AllpowersBLE:
             s[x] = full[x]
 
         s[7] = 0
+        # When writing, the order of bits is not the same as when reading
+        #Commented options do nothing on AllPowers R600 V2
+        s[7] = s[7] ^ (1 << 0) if self.dc_on     else s[7] & ~(1 << 0)
+        s[7] = s[7] ^ (1 << 1) if self.ac_on     else s[7] & ~(1 << 1)
+        #s[7] = s[7] ^ (1 << 2) if self.close_ble else s[7] & ~(1 << 2)
+        s[7] = s[7] ^ (1 << 3) if self.f60hz_on  else s[7] & ~(1 << 3)
+        #s[7] = s[7] ^ (1 << 4) if self.beep_on   else s[7] & ~(1 << 4)
+        s[7] = s[7] ^ (1 << 5) if self.light_on  else s[7] & ~(1 << 5)
+        #s[7] = s[7] ^ (1 << 6) if self.screen_on else s[7] & ~(1 << 6) 
+        #s[7] = s[7] ^ (1 << 7) if self.voice_on  else s[7] & ~(1 << 7)
 
-        s[7] = s[7] ^ (1 << 5) if self.light_on else s[7] & ~(1 << 5)
-        s[7] = s[7] ^ (1 << 0) if self.dc_on else s[7] & ~(1 << 0)
-        s[7] = s[7] ^ (1 << 1) if self.ac_on else s[7] & ~(1 << 1)
+        # Calculate checksum
+        s[8] = 0
+        s[8] = self.crc(s)
+        
+        if self._client is not None:
+            await self._client.write_gatt_char(CHARACTERISTIC_WRITE, s)
 
-        # I'm sure this checksum algo isn't complete/correct,
-        # but it certainly works for all the scenarios we care about
-        s[8] = 113 - s[7]
-        if self.ac_on:
-            s[8] = s[8] + 4
+    async def _change_status_to_device2(self) -> None:
+        full = bytes.fromhex("a56500b1010202000171")
+        s = bytearray(10)
+        for x in range(10):
+            s[x] = full[x]
+
+        s[7] = 0
+        #Commented options do nothing on AllPowers R600 V2
+        s[7] = s[7] ^ (1 << 0) if self.ecoMode_on else s[7] & ~(1 << 0)
+        s[7] = (s[7] & ~0b110) | (self.chargingMode << 1)
+        #s[7] = s[7] ^ (1 << 3) if self.acMode_on else s[7] & ~(3 << 0)
+        #s[7] = s[7] ^ (1 << 4) if self.carPortEn_on else s[7] & ~(4 << 0)
+        s[8] = self.ecoTime
+        
+        # Calculate checksum
+        s[9]=0
+        s[9]=self.crc(s)
 
         if self._client is not None:
             await self._client.write_gatt_char(CHARACTERISTIC_WRITE, s)
@@ -165,6 +255,75 @@ class AllpowersBLE:
         """Set the current value of the DC."""
         self._state.dc_on = enabled
         await self._change_status_to_device()
+
+    async def set_f60hz(self, enabled: bool) -> None:
+        """Set the current value of frequency."""
+        self._state.f60hz_on = enabled
+        await self._change_status_to_device()
+
+    async def set_beep(self, enabled: bool) -> None:
+        """Set the current value of beep."""
+        self._state.beep_on = enabled
+        await self._change_status_to_device()
+
+    async def set_screen(self, enabled: bool) -> None:
+        """Set the current value of the screen."""
+        self._state.screen_on = enabled
+        await self._change_status_to_device()
+
+    async def set_voice(self, enabled: bool) -> None:
+        """Set the current value of the voice."""
+        self._state.voice_on = enabled
+        await self._change_status_to_device()
+
+    async def set_close_ble(self, enabled: bool) -> None:
+        """Set the current value of the Bluetoth?"""
+        self._state.close_ble = enabled
+        await self._change_status_to_device()
+
+    async def set_ecoMode_on(self, enabled: bool) -> None:
+        """Set the current value of the Bluetoth?"""
+        self._state.ecoMode_on = enabled
+        await self._change_status_to_device2()
+
+    async def set_ecoTime(self, newEcoTime: int) -> None:
+        """Set the current value of ecoTime"""
+        #Only certain values are allowed
+        allowedValues=[1,2,4,6]
+        res=self.verifyPositiveValueOnList(newEcoTime,allowedValues)
+        if res!=-1:
+            self._state.ecoTime = newEcoTime
+            await self._change_status_to_device2()
+        else:
+            _LOGGER.error("Invalid ecoTime. It must be one of %s. Ignoring command",allowedValues)
+
+    async def set_chargingMode(self, newChargingMode: int) -> None:
+        """Set the current value of ecoTime"""
+        #Only certain values are allowed
+        allowedValues=[0,1,2]
+        res=self.verifyPositiveValueOnList(newChargingMode,allowedValues)
+        if res!=-1:
+            self._state.chargingMode = newChargingMode
+            await self._change_status_to_device2()
+        else:
+            _LOGGER.error("Invalid chargingMode. It must be one of %s. Ignoring command",allowedValues)
+
+    async def set_acMode_on(self, enabled: bool) -> None:
+        """Set the current value of the Bluetoth?"""
+        self._state.acMode_on = enabled
+        await self._change_status_to_device2()
+
+    async def set_carPortEn_on(self, enabled: bool) -> None:
+        """Set the current value of the Bluetoth?"""
+        self._state.carPortEn_on = enabled
+        await self._change_status_to_device2()
+
+    def verifyPositiveValueOnList(self,value: int, allowedValues) -> int:
+        res=-1
+        for item in allowedValues:
+            if item==value:
+                res = value
+        return res
 
     async def stop(self) -> None:
         """Stop the Allpowers BLE."""
@@ -258,36 +417,87 @@ class AllpowersBLE:
     def _notification_handler(self, _sender: int, data: bytearray) -> None:
         """Handle notification responses."""
         _LOGGER.debug("%s: Notification received: %s", self.name, data.hex())
+        
+        if (len(data)==16):
+            self._buf += data
 
-        self._buf += data
+            battery_percentage = data[8]
+            dc_on     = data[7] >> 0 & 1 == 1
+            ac_on     = data[7] >> 1 & 1 == 1
+            f60hz_on  = data[7] >> 2 & 1 == 1
+            beep_on   = data[7] >> 3 & 1 == 1
+            torch_on  = data[7] >> 4 & 1 == 1
+            screen_on = data[7] >> 5 & 1 == 1
+            voice_on  = data[7] >> 6 & 1 == 1
+            output_power = (256 * data[11]) + data[12]
+            input_power = (256 * data[9]) + data[10]
+            minutes_remaining = (256 * data[13]) + data[14]
+            
+            self._state = AllpowersState(
+                ac_on=ac_on,
+                dc_on=dc_on,
+                f60hz_on=f60hz_on,
+                beep_on=beep_on,
+                light_on=torch_on,
+                screen_on=screen_on,
+                voice_on=voice_on,
+                percent_remain=battery_percentage,
+                minutes_remain=minutes_remaining,
+                watts_export=output_power,
+                watts_import=input_power,
+                ecoMode_on=self.ecoMode_on,
+                chargingMode=self.chargingMode,
+                acMode_on=self.acMode_on,
+                carPortEn_on=self.carPortEn_on,
+                ecoTime=self.ecoTime,
+                chargeTime=self.chargeTime
+            )
 
-        battery_percentage = data[8]
-        dc_on = data[7] >> 0 & 1 == 1
-        ac_on = data[7] >> 1 & 1 == 1
-        torch_on = data[7] >> 4 & 1 == 1
-        output_power = (256 * data[11]) + data[12]
-        input_power = (256 * data[9]) + data[10]
-        minutes_remaining = (256 * data[13]) + data[14]
+        else:
+            if (len(data)==14):
+                self._buf += data
+                ecoTime = data[8]
+                ecoMode_on  = data[7] >> 0 & 1 == 1
+                # chargingMode: 0->Mute; 1->Standard; 2->Fast
+                chargingMode= (data[7] & 0b110) >> 1
+                acMode_on   = data[7] >> 3 & 1 == 1
+                carPortEn_on= data[7] >> 4 & 1 == 1
+                chargeTime = (256 * data[9]) + data[10]
 
-        self._state = AllpowersState(
-            ac_on=ac_on,
-            dc_on=dc_on,
-            light_on=torch_on,
-            percent_remain=battery_percentage,
-            minutes_remain=minutes_remaining,
-            watts_export=output_power,
-            watts_import=input_power,
-        )
+                self._state = AllpowersState(
+                    ac_on=self.ac_on,
+                    dc_on=self.dc_on,
+                    f60hz_on=self.f60hz_on,
+                    beep_on=self.beep_on,
+                    light_on=self.light_on,
+                    screen_on=self.screen_on,
+                    voice_on=self.voice_on,
+                    percent_remain=self.percent_remain,
+                    minutes_remain=self.minutes_remain,
+                    watts_export=self.watts_export,
+                    watts_import=self.watts_import,
+                    ecoMode_on=ecoMode_on,
+                    chargingMode=chargingMode,
+                    acMode_on=acMode_on,
+                    carPortEn_on=carPortEn_on,
+                    ecoTime=ecoTime,
+                    chargeTime=chargeTime
+                )
 
-        self._fire_callbacks()
+            else:
+                _LOGGER.debug("%s: Invalid size Notification received: %s", self.name, len(data))
+                
+        if (len(data)==16 or len(data)==14):
+            self._fire_callbacks()
 
-        _LOGGER.debug(
-            "%s: Notification received; RSSI: %s: %s %s",
-            self.name,
-            self.rssi,
-            data.hex(),
-            self._state,
-        )
+            _LOGGER.debug(
+                "%s: Notification received; RSSI: %s: %s %s",
+                self.name,
+                self.rssi,
+                data.hex(),
+                self._state,
+            )
+
 
     def _disconnected(self, client: BleakClientWithServiceCache) -> None:
         """Disconnected callback."""
